4056ea5b960f1e170310a41b5361309693be4e13
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2023-03-15 15:48:43 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      const blogs = await blogService.getAll()
+      setBlogs(blogs)
+    }
+
+    fetchBlogs()
+  }, [])
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Blog.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Blog = ({ blog }) => (
+  <div>
+    {blog.title} {blog.author}
+  </div>
+)
+
+Blog.propTypes = {
+  blog: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+  }).isRequired,
+}
+
+export default Blog
